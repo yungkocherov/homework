56ac3b8d3b7d70cc1829e8948f2c79189a3892f6
yungkocherov <ikocherov@mail.ru>
2017-10-24 19:40:43 +0300

    fix

--- q1/q12.py
+++ q1/q12.py
@@ -1,3 +1,10 @@
-from qq import del00 as check1
-x = int(input())
-print(*check1(x))
+def del0(x):
+    a = []
+    for i in range(1, x + 1):
+        if x % i == 0: a.append(i)
+    return a
+
+
+if __name__ == "__main__":
+    x = int(input())
+    print(*del0(x))
--- q1/q13.py
+++ q1/q13.py
@@ -1,4 +1,6 @@
-x=int(input())
-from qq import del00 as check2
-if len(check2(x))>2: print('False')
-else: print('True')
+from q12 import del0 as check2
+x = int(input())
+if len(check2(x)) > 2:
+    print('False')
+else:
+    print('True')
