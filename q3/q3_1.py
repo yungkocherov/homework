83f00f0a9a18d5aa3456c64e75f1d258e935672d
yungkocherov <ikocherov@mail.ru>
2017-11-03 21:39:41 +0300

    fix

--- q3/q3_1.py
+++ q3/q3_1.py
@@ -2,33 +2,37 @@ class StackSkobochek:
     def __init__(self, string):
         self.string = string
         self.stack = "."
+
+    @property
     def check1(self):
         for i in range(len(self.string)):
-            if self.string[i] == "(" or self.string[i] == "{" or self.string[i] == "[" or self.string[i] =="]" or self.string[i] == "}" or self.string[i] == ")":
+            if self.string[i] == "(" or self.string[i] == "{" or self.string[i] == "[" or self.string[i] == "]" or self.string[i] == "}" or self.string[i] == ")":
                 self.stack += self.string[i]
+
             if self.stack[len(self.stack) - 1] == "}":
                 if self.stack[len(self.stack) - 2] == "{":
                     self.stack = self.stack[:-2]
                     continue
                 else:
                     return "Invalid combination"
-            if self.stack[len(self.stack) - 1] == "}":
-                if self.stack[len(self.stack) - 2] == "{":
+            if self.stack[len(self.stack) - 1] == ")":
+                if self.stack[len(self.stack) - 2] == "(":
                     self.stack = self.stack[:-2]
                     continue
                 else:
                     return "Invalid combination"
-            if self.stack[len(self.stack) - 1] == "}":
-                    if self.stack[len(self.stack) - 2] == "{":
-                        self.stack = self.stack[:-2]
-                        continue
-                    else:
-                        return "Invalid combination"
+            if self.stack[len(self.stack) - 1] == "]":
+                if self.stack[len(self.stack) - 2] == "[":
+                    self.stack = self.stack[:-2]
+                    continue
+                else:
+                    return "Invalid combination"
 
         if self.stack == ".":
             return "Valid combination"
         else:
             return "invalid combination"
+
 
 str = input()
 o = StackSkobochek(str)
